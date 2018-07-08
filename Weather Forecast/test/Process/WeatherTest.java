ad3dfec8550c1a89efce06585a7e6b6287c7a1f0
Kurniandha S.Y <ykurniandha@gmail.com>
2018-07-08 23:36:00 +0700

    Add Unit Test

---
+++ Weather Forecast/test/Process/WeatherTest.java
@@ -0,0 +1,276 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Process;
+
+import java.util.ArrayList;
+import org.json.simple.JSONObject;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kurniandha
+ */
+public class WeatherTest {
+    
+    public WeatherTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getFinalURL method, of class Weather.
+     */
+    @Test
+    public void testGetFinalURL() {
+        System.out.println("getFinalURL");
+        Weather instance = null;
+        String expResult = "";
+
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of getWeatherData method, of class Weather.
+     */
+    @Test
+    public void testGetWeatherData() {
+        System.out.println("getWeatherData");
+        String cityID = "";
+        Weather instance = null;
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of parseData method, of class Weather.
+     */
+    @Test
+    public void testParseData() {
+        System.out.println("parseData");
+        Weather instance = null;
+        JSONObject expResult = null;
+
+        // TODO review the generated test code and remove the default call to fail.
+     
+    }
+
+    /**
+     * Test of preCityFinder method, of class Weather.
+     */
+    @Test
+    public void testPreCityFinder() {
+        System.out.println("preCityFinder");
+        Weather instance = null;
+
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of findCityID method, of class Weather.
+     */
+    @Test
+    public void testFindCityID() {
+        System.out.println("findCityID");
+        Weather instance = null;
+        ArrayList<JSONObject> expResult = null;
+       
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of crawlData method, of class Weather.
+     */
+    @Test
+    public void testCrawlData() throws Exception {
+        System.out.println("crawlData");
+        Weather instance = null;
+
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getCityName method, of class Weather.
+     */
+    @Test
+    public void testGetCityName() {
+        System.out.println("getCityName");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+   
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyWindSpeed method, of class Weather.
+     */
+    @Test
+    public void testGetDailyWindSpeed() {
+        System.out.println("getDailyWindSpeed");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of getDailyWindDirection method, of class Weather.
+     */
+    @Test
+    public void testGetDailyWindDirection() {
+        System.out.println("getDailyWindDirection");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of getDailyTemp method, of class Weather.
+     */
+    @Test
+    public void testGetDailyTemp() {
+        System.out.println("getDailyTemp");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyWeatherMain method, of class Weather.
+     */
+    @Test
+    public void testGetDailyWeatherMain() {
+        System.out.println("getDailyWeatherMain");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyWeatherDetail method, of class Weather.
+     */
+    @Test
+    public void testGetDailyWeatherDetail() {
+        System.out.println("getDailyWeatherDetail");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyDaySimplified method, of class Weather.
+     */
+    @Test
+    public void testGetDailyDaySimplified() {
+        System.out.println("getDailyDaySimplified");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+   
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyDayFull method, of class Weather.
+     */
+    @Test
+    public void testGetDailyDayFull() {
+        System.out.println("getDailyDayFull");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyMonth method, of class Weather.
+     */
+    @Test
+    public void testGetDailyMonth() {
+        System.out.println("getDailyMonth");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyYear method, of class Weather.
+     */
+    @Test
+    public void testGetDailyYear() {
+        System.out.println("getDailyYear");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of getDailyDate method, of class Weather.
+     */
+    @Test
+    public void testGetDailyDate() {
+        System.out.println("getDailyDate");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+
+        // TODO review the generated test code and remove the default call to fail.
+  
+    }
+
+    /**
+     * Test of getDateTime method, of class Weather.
+     */
+    @Test
+    public void testGetDateTime() {
+        System.out.println("getDateTime");
+        JSONObject data = null;
+        Weather instance = null;
+        String expResult = "";
+
+
+    }
+    
+}
