0b80a9aed2356f15ea376c5ce57aeb3fb5657d97
Kurniandha S.Y <ykurniandha@gmail.com>
2018-07-08 22:59:22 +0700

    Final Minimal

--- Weather Forecast/src/Main/Main.java
+++ Weather Forecast/src/Main/Main.java
@@ -6,12 +6,14 @@
 package Main;
 
 import Process.Weather;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 /**
  *
  * @author kurniandha
  */
 public class Main extends javax.swing.JFrame {
-
     /**
      * Creates new form Main
      */
@@ -33,6 +35,16 @@ public class Main extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField6 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Weather Forecast");
@@ -62,7 +74,7 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1))
+                        .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 389, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jButton1)))
@@ -80,15 +92,91 @@ public class Main extends javax.swing.JFrame {
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Result"));
 
+        jLabel2.setText("City :");
+
+        jTextField2.setToolTipText("");
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Temperature : ");
+
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
+
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Weather :");
+
+        jLabel5.setText("Wind Speed : ");
+
+        jTextField5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField5ActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("Wind Direction :");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 518, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField2)
+                            .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 144, Short.MAX_VALUE)
+                            .addComponent(jTextField4)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField6)
+                            .addComponent(jTextField5))))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 257, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(119, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -119,8 +207,55 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        // TODO add your handling code here:
+        String cityName = this.jTextField1.getText();
+        
+        Weather weather = new Weather(cityName);
+        weather.preCityFinder();
+        System.out.println("HAHA");
+        ArrayList<JSONObject> cityResult = weather.findCityID();
+        String found = "";
+        System.out.println(cityResult.size());
+        
+        if (cityResult.size() >= 1) {
+            found = cityResult.get(0).get("id").toString();
+            System.out.println(found);
+            //repaint();
+        } 
+        
+        if (found.equals("MANY")) {
+           // result
+        } else if (found.length() > 0) {
+            weather.getWeatherData(found);
+            JSONObject now;
+            now = weather.parseData();
+            //JSONObject now = 
+            System.out.println(weather.getCityName(now));
+            this.jTextField2.setText(weather.getCityName(now));
+            JSONArray a = (JSONArray) now.get("list");
+            JSONObject b = (JSONObject) a.get(0);
+            this.jTextField3.setText(weather.getDailyTemp(b) + "\u00b0c");
+            System.out.println(weather.getDailyPressure(b));
+            this.jTextField4.setText(weather.getDailyWeatherMain(b));
+            this.jTextField5.setText(weather.getDailyWindSpeed(b) + " m/s");
+            this.jTextField6.setText(weather.getDailyWindDirection(b) + " degree");
+        }
     }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField3ActionPerformed
+
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField4ActionPerformed
+
+    private void jTextField5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField5ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -160,8 +295,18 @@ public class Main extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
     // End of variables declaration//GEN-END:variables
 }
--- Weather Forecast/src/Process/Weather.java
+++ Weather Forecast/src/Process/Weather.java
@@ -13,7 +13,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -30,13 +33,15 @@ public class Weather {
     JSONArray citylist;
     JSONParser parser;
     private String finalURL;
-
+    private ArrayList<JSONObject> data;
+    
     public String getFinalURL() {
         return finalURL;
     }
     
     public Weather(String name) {
         cityName = name;
+        data = new ArrayList<>();
     }
     
     public void getWeatherData(String cityID) {
@@ -53,7 +58,7 @@ public class Weather {
         JSONObject data = new JSONObject();
         
         try {
-            data = (JSONObject) parsernow.parse (new FileReader("Data/data.json"));
+            data = (JSONObject) parsernow.parse (new FileReader("result.json"));
         } catch (FileNotFoundException e) {
           System.out.println("Cannot find file!");
         } catch (ParseException e) {
@@ -69,7 +74,7 @@ public class Weather {
         this.parser = new JSONParser();
         
         try {
-            Object obj = this.parser.parse(new FileReader("Data/data.json"));
+            Object obj = this.parser.parse(new FileReader("city.list.json"));
             this.citylist = (JSONArray) obj;
         } catch (FileNotFoundException e) {
             System.out.println("Cannot find file");
@@ -109,7 +114,7 @@ public class Weather {
             }
             input.close();
             
-            PrintWriter output = new PrintWriter("Data/data.json");
+            PrintWriter output = new PrintWriter("result.json");
             output.write(responseStr.toString());
             output.flush();
             output.close();
@@ -117,4 +122,137 @@ public class Weather {
             System.out.println("Error");
         }
     }
+    
+    public void pushIndex(JSONObject obj) {
+        this.data.add(obj);
+    }
+
+    public JSONObject getData(int idx) {
+        return this.data.get(idx);
+    }
+
+    public String getCityName(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("city");
+        return detail.get("name").toString();
+    }
+
+    public String getDailyWindSpeed(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("wind");
+        return detail.get("speed").toString();
+    }
+
+    public String getDailyWindDirection(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("wind");
+        return detail.get("deg").toString();
+    }
+
+    public String getDailyCloud(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("clouds");
+        return detail.get("all").toString();
+    }
+
+    public String getDailyHumidity(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("main");
+        return detail.get("humidity").toString();
+    }
+
+    public String getDailyPressure(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("main");
+        return detail.get("pressure").toString();
+    }
+
+    public String getDailyTemp(JSONObject data) {
+        JSONObject detail = (JSONObject) data.get("main");
+        Double temp = (((Number)detail.get("temp")).doubleValue()) - 273.15;
+        DecimalFormat df = new DecimalFormat("#.#");
+        return String.valueOf(df.format(temp));
+    }
+
+    public String getDailyWeatherMain(JSONObject data) {
+        JSONArray weather = (JSONArray) data.get("weather");
+        JSONObject detail = (JSONObject) weather.get(0);
+        return detail.get("main").toString();
+    }
+
+    public String getDailyWeatherDetail(JSONObject data) {
+        JSONArray weather = (JSONArray) data.get("weather");
+        JSONObject detail = (JSONObject) weather.get(0);
+        String input = detail.get("description").toString();
+        String output = input.substring(0, 1).toUpperCase() + input.substring(1);
+        return output;
+    }
+
+    public String getDailyDaySimplified(JSONObject data) {
+        String result = "";
+        String dateTime = data.get("dt_txt").toString();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(dateTime);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("E");
+            result = dateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getDailyDayFull(JSONObject data) {
+        String result = "";
+        String dateTime = data.get("dt_txt").toString();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(dateTime);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE");
+            result = dateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getDailyMonth(JSONObject data) {
+        String result = "";
+        String dateTime = data.get("dt_txt").toString();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(dateTime);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM");
+            result = dateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getDailyYear(JSONObject data) {
+        String result = "";
+        String dateTime = data.get("dt_txt").toString();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(dateTime);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
+            result = dateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getDailyDate(JSONObject data) {
+        String result = "";
+        String dateTime = data.get("dt_txt").toString();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(dateTime);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("d");
+            result = dateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getDateTime(JSONObject data) {
+        return data.get("dt_txt").toString();
+  }
 }
